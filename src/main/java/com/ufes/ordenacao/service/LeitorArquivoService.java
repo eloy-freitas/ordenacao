656f334c919a9159e80b639da09c4c0a94fe635c
eloy-freitas <freitaseloy.1@gmail.com>
2022-10-15 14:50:42 -0300

    feat: implementação LeitorArquivoService

---
+++ src/main/java/com/ufes/ordenacao/service/LeitorArquivoService.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ufes.ordenacao.service;
+
+import com.ufes.ordenacao.business.leitor_arquivo.LeitorArquivoCsvHandler;
+import com.ufes.ordenacao.business.leitor_arquivo.LeitorArquivoProcessor;
+import com.ufes.ordenacao.business.leitor_arquivo.LeitorArquivoTxtHandler;
+import java.util.List;
+
+/**
+ *
+ * @author eloy
+ */
+public class LeitorArquivoService {
+    private LeitorArquivoProcessor arquivoProcessor ;
+    
+    public LeitorArquivoService() {
+        this.arquivoProcessor = new LeitorArquivoProcessor();
+        this.arquivoProcessor.addLeitorArquivoHandler(new LeitorArquivoCsvHandler());
+        this.arquivoProcessor.addLeitorArquivoHandler(new LeitorArquivoTxtHandler());
+    }
+    
+    public List<Double> processarArquivo(String path) throws Exception{
+        return  arquivoProcessor.handleRequest(path);
+    }
+    
+}
